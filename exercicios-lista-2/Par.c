ccee47d1e97489c9409351e19c27abe848cd7c94
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2020-09-26 21:58:24 -0300

    adiciona comentario e melhora a indentação

--- exercicios-lista-2/Par.c
+++ exercicios-lista-2/Par.c
@@ -1,18 +1,20 @@
+//Algoritmo que verificar se o numero digitado pelo usuario é par ou não
 #include <stdio.h>
 
 int main() {
     int numero = 0;
-    printf("Digite um numero: ");
-    scanf("%d", &numero);
+    
+    printf ("Digite um numero: ");
+    scanf ("%d", &numero);
 
-    if(numero != 0){
-        if(numero % 2 == 0){
-            printf("\nO numero %d e par!\n", numero);
-        }else {
-            printf("\nO numero %d nao e par!\n", numero);
+    if (numero != 0) {
+        if (numero % 2 == 0) {
+        	printf("\nO numero %d e par!\n", numero);
+        } else {
+            printf ("\nO numero %d nao e par!\n", numero);
             }
-    }else {
-        printf("\nEntrada INVALIDA!\n");
+    } else {
+        printf ("\nEntrada INVALIDA!\n");
         }
 
 return 0;
