72d3c05e8127639d5c0f114d4f67f423b777954c
Vitor Figueiredo <61101830+vitorfigueiredopb@users.noreply.github.com>
2020-10-21 00:28:40 -0300

    Add files via upload

---
+++ exercicios-lista-5/pergunta1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+	struct horario {
+		int horas, minutos, segundos;
+	};
+	
+	struct data {
+		int dia, mes, ano;
+	};
+	
+	struct.compromisso {
+		struct data esta_data;
+		struct horario este_horario;
+		char descricao[80];
+	};
+	
+	return 0;
+}
